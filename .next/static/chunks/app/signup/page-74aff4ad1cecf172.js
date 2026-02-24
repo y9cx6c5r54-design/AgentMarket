89346104876405c116f4ad83d9821a2d1f75ce3a
Clawdia <clawdia@Clawdias-MacBook-Pro.local>
2026-02-24 14:21:08 -0800

    chore: remove build artifacts from git

--- .next/static/chunks/app/signup/page-74aff4ad1cecf172.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[966,301,3,996,821,162,521,669,55,626,240,964,906],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
